b0721380024c1b9c353ef9f5ad562ce80fdf3586
Martin Deinhofer <martin.deinhofer@technikum-wien.at>
2016-06-24 11:48:37 +0200

    fixed ClassPathLibraryLoader: HID_LIB_NAMES did not reflect actual native lib paths structure. Improved searching of native lib for platform by skipping other OSes. changed version to 1.1libLoaderPatch changed HIDAPITest to use ClassPathLibraryLoader

--- src/com/codeminders/hidapi/ClassPathLibraryLoader.java
+++ src/com/codeminders/hidapi/ClassPathLibraryLoader.java
@@ -8,21 +8,26 @@ import java.io.OutputStream;
 
 public class ClassPathLibraryLoader {
 
-    private static final String[] HID_LIB_NAMES = {
-	        "/native/linux/libhidapi-jni-64.so",
-	        "/native/linux/libhidapi-jni-32.so",
-	        "/native/mac/libhidapi-jni-64.jnilib",
-	        "/native/mac/libhidapi-jni-32.jnilib",
-	        "/native/win/hidapi-jni-64.dll",
-	        "/native/win/hidapi-jni-32.dll"
+    public static String[] HID_LIB_NAMES = {
+	        "/native/linux/x86_64/libhidapi-jni-64.so",
+	        "/native/linux/x86/libhidapi-jni-32.so",
+	        "/native/linux/arm/libhidapi-jni-32.so",
+	        "/native/macosx/x86_64/libhidapi-jni-64.jnilib",
+	        "/native/macosx/x86/libhidapi-jni-32.jnilib",
+	        "/native/windows/x86_64/hidapi-jni-64.dll",
+	        "/native/windows/x86/hidapi-jni-32.dll"
 	};
-	  
+	      
 	public static boolean loadNativeHIDLibrary()
         {
 		  boolean isHIDLibLoaded = false;
-		  
+		  String osName=getOsName();
     	  for(String path : HID_LIB_NAMES)
           {
+    		  if(!path.matches(".*/"+osName+"/.*")) {
+    			  //System.out.println("Skipping path "+path+", for OS: "+osName);
+    			  continue;
+    		  }
 		        try {
 		                // have to use a stream
 		                InputStream in = ClassPathLibraryLoader.class.getResourceAsStream(path);
@@ -60,5 +65,47 @@ public class ClassPathLibraryLoader {
     	  
     	return isHIDLibLoaded;  
     }
+    
+	private static String OS = System.getProperty("os.name").toLowerCase();
+	 
+	/**
+	 * Running on windows? 
+	 * @return true: yes
+	 */
+	public static boolean isWindows() {
+		return (OS.indexOf("win") >= 0);
+	}
+ 
+	/**
+	 * Running on Mac OS X?
+	 * @return true: yes
+	 */
+	public static boolean isMac() {
+		return (OS.indexOf("mac") >= 0);
+	}
+ 
+	/**
+	 * Running on Linux or Unix?
+	 * @return true: yes
+	 */
+	public static boolean isUnix() {
+		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
+	}
+	
+	/**
+	 * Returns the operating system name according to my convention :-)
+	 * @return
+	 */
+	public static String getOsName() {
+		if(isWindows()) {
+			return "windows";
+		} else if(isMac()) {
+			return "macosx";
+		} else if(isUnix()) {
+			return "linux";
+		}
+		return "unknown";
+	}
+    
 
 }
